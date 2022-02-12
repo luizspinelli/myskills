dbf2bb68a7ad622df83e4a73dc89ddd52013313d
Luiz Spinelli <luiz.luchi@gmail.com>
2022-02-12 12:11:08 -0300

    home page

---
+++ src/pages/Home.js
@@ -0,0 +1,67 @@
+import React, { useState, useCallback } from "react";
+import {
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+import { Button } from "../components/Button";
+import { SkillCard } from "../components/SkillCard";
+
+const Home = () => {
+  const [newSkill, setNewSkill] = useState("");
+
+  const [skills, setSkills] = useState([]);
+
+  const handleAddSkill = useCallback(() => {
+    setSkills((old) => [...old, newSkill]);
+    setNewSkill("");
+  }, [newSkill]);
+
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}> Welcome, Luiz </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="New skill"
+          placeholderTextColor="#555"
+          onChangeText={setNewSkill}
+          value={newSkill}
+        />
+        <Button onPress={handleAddSkill} text="Add" />
+        <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
+        {skills.map((skill, n) => (
+          <SkillCard key={n} text={skill} />
+        ))}
+      </SafeAreaView>
+    </>
+  );
+};
+
+export { Home };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121015",
+    paddingVertical: 70,
+    paddingHorizontal: 30,
+  },
+  title: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#1f1e25",
+    color: "#fff",
+    fontSize: 18,
+    padding: Platform.OS === "ios" ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+});
